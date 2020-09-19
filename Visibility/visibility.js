ec09c50bf090708e9f5a5a84cde32d92e1d853e6
BanerjeeProdipta <prodiptaj@live.com>
2020-09-19 15:14:09 +0600

    9

---
+++ Visibility/visibility.js
@@ -0,0 +1,21 @@
+Vue.component('message', {
+    props: ['title', 'body'],
+    data() {
+        return {
+            isVisible: true
+        }
+    },
+    template: `
+        <article class = "message is-info" v-show="isVisible">
+        <div class = "message-header" >
+        <p> {{title}} </p> 
+        <button class = "delete" aria-label = "delete" @click="isVisible= false" ></button> 
+        </div> 
+        <div class = "message-body" >
+            {{body}}
+        </div> 
+    </article>`,
+});
+var app = new Vue({
+    el: '#root',
+})
