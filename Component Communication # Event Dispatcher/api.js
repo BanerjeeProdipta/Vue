a391338d64f8db6e7b57080084f81d659bcca7e7
BanerjeeProdipta <prodiptaj@live.com>
2020-09-20 20:19:05 +0600

    13

---
+++ Component Communication # Event Dispatcher/api.js
@@ -0,0 +1,36 @@
+window.EventDispatcher = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+};
+
+Vue.component('coupon', {
+    template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
+
+    methods: {
+        onCouponApplied() {
+            EventDispatcher.fire('applied');
+        }
+    }
+});
+
+new Vue({
+    el: '#root',
+
+    data: {
+        couponApplied: false
+    },
+
+    created() {
+        EventDispatcher.listen('applied', () => this.couponApplied = true);
+    }
+
+});
