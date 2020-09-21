f981db9210fd0e126661ba995b4f5d2b806f57b7
BanerjeeProdipta <prodiptaj@live.com>
2020-09-21 20:11:23 +0600

    17

---
+++ lesson/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>The Count is: {{count}}</h2>
+    <button @click= "count += 1">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  }
+}
+</script>
